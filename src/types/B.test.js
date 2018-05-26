5270598214a2876833fe1d8ef01f8051d506ea1c
tleef <the.tleef@gmail.com>
2018-05-26 12:48:47 -0400

    Add type tests

---
+++ src/types/B.test.js
@@ -0,0 +1,53 @@
+
+/* eslint-env mocha */
+
+import chai from 'chai'
+
+import B from './B'
+
+const expect = chai.expect
+
+describe('B', () => {
+  describe('B.toDynamo()', () => {
+    it('should return a B AttributeValue', () => {
+      const av = B.toDynamo(Buffer.from('test'))
+
+      expect(av).to.be.an.instanceof(Object)
+      expect(av.B).to.be.a('string')
+    })
+
+    it('should base64 encode the value', () => {
+      const av = B.toDynamo(Buffer.from('test'))
+
+      expect(av.B).to.equal('dGVzdA==')
+    })
+  })
+
+  describe('B.fromDynamo()', () => {
+    it('should return a Buffer', () => {
+      const b = B.fromDynamo({B: 'dGVzdA=='})
+
+      expect(b).to.be.an.instanceof(Buffer)
+    })
+
+    it('should decode the base64 value', () => {
+      const b = B.fromDynamo({B: 'dGVzdA=='})
+
+      expect(b).to.deep.equal(Buffer.from('test'))
+    })
+  })
+
+  describe('B.validate()', () => {
+    it('should accept a Buffer', () => {
+      const bool = B.validate(Buffer.from('test'))
+
+      expect(bool).to.equal(true)
+    })
+
+    it('should reject a string', () => {
+      const bool = B.validate('test')
+
+      expect(bool).to.equal(false)
+    })
+  })
+})
---
+++ src/types/Json.test.js
@@ -0,0 +1,53 @@
+
+/* eslint-env mocha */
+
+import chai from 'chai'
+
+import Json from './Json'
+
+const expect = chai.expect
+
+describe('Json', () => {
+  describe('Json.toDynamo()', () => {
+    it('should return a S AttributeValue', () => {
+      const av = Json.toDynamo({key: 'value'})
+
+      expect(av).to.be.an.instanceof(Object)
+      expect(av.S).to.be.a('string')
+    })
+
+    it('should correctly set S', () => {
+      const av = Json.toDynamo({key: 'value'})
+
+      expect(av.S).to.equal(JSON.stringify({key: 'value'}))
+    })
+  })
+
+  describe('Json.fromDynamo()', () => {
+    it('should return an Object', () => {
+      const o = Json.fromDynamo({S: JSON.stringify({key: 'value'})})
+
+      expect(o).to.be.an.instanceof(Object)
+    })
+
+    it('should return correct value', () => {
+      const o = Json.fromDynamo({S: JSON.stringify({key: 'value'})})
+
+      expect(o).to.deep.equal({key: 'value'})
+    })
+  })
+
+  describe('Json.validate()', () => {
+    it('should accept an Object', () => {
+      const bool = Json.validate({key: 'value'})
+
+      expect(bool).to.equal(true)
+    })
+
+    it('should reject string', () => {
+      const bool = Json.validate('test')
+
+      expect(bool).to.equal(false)
+    })
+  })
+})
