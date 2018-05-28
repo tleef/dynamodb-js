6ffc7ad5ee9ac43f4790140291607243dd6f89f2
tleef <the.tleef@gmail.com>
2018-05-28 05:46:41 -0400

    progress

--- src/DynamoDB.js
+++ src/DynamoDB.js
@@ -17,9 +17,9 @@ export default class DynamoDB {
     let conditionExpression = ''
 
     Object.keys(this.keySchema).forEach((name) => {
-      let key = makeKey()
-      while (expressionAttributeNames.hasOwnProperty(key)) {
-        key = makeKey()
+      let key = this.makeKey()
+      while (expressionAttributeNames.hasOwnProperty(`#${key}`)) {
+        key = this.makeKey()
       }
 
       expressionAttributeNames[`#${key}`] = name
@@ -38,7 +38,7 @@ export default class DynamoDB {
       ExpressionAttributeNames: expressionAttributeNames
     }
 
-    return dynamodb.putItem(params).promise()
+    return this.dynamodb().putItem(params).promise()
   }
 
   async put (o) {
@@ -49,33 +49,130 @@ export default class DynamoDB {
       TableName: this.tableName
     }
 
-    return dynamodb.putItem(params).promise()
+    return this.dynamodb().putItem(params).promise()
   }
 
-  async replace () {
+  async replace (o) {
+    const item = this.itemSchema.toDynamo(o)
+    const expressionAttributeNames = {}
+    let conditionExpression = ''
 
+    Object.keys(this.keySchema).forEach((name) => {
+      let key = this.makeKey()
+      while (expressionAttributeNames.hasOwnProperty(`#${key}`)) {
+        key = this.makeKey()
+      }
+
+      expressionAttributeNames[`#${key}`] = name
+
+      if (conditionExpression) {
+        conditionExpression += ' AND '
+      }
+
+      conditionExpression += `attribute_exists(#${key})`
+    })
+
+    const params = {
+      Item: item,
+      TableName: this.tableName,
+      ConditionExpression: conditionExpression,
+      ExpressionAttributeNames: expressionAttributeNames
+    }
+
+    return this.dynamodb().putItem(params).promise()
   }
 
-  async update () {
+  async update (o) {
+    const item = this.itemSchema.toDynamo(o)
+    const expressionAttributeNames = {}
+    const expressionAttributeValues = {}
+    let updateExpression = ''
+    let conditionExpression = ''
 
+    Object.keys(item).forEach((name) => {
+      let key = this.makeKey()
+      while (expressionAttributeNames.hasOwnProperty(`#${key}`)) {
+        key = this.makeKey()
+      }
+
+      expressionAttributeNames[`#${key}`] = name
+
+      if (this.keySchema.hasOwnProperty(name)) {
+        if (conditionExpression) {
+          conditionExpression += ' AND '
+        }
+
+        conditionExpression += `attribute_exists(#${key})`
+      } else {
+        expressionAttributeValues[`:${key}`] = item[name]
+
+        if (!updateExpression) {
+          updateExpression = `SET #${key} = :${key}`
+        } else {
+          updateExpression += `, #${key} = :${key}`
+        }
+      }
+    })
+
+    const params = {
+      Key: this.keySchema.toDynamo(o),
+      TableName: this.tableName,
+      UpdateExpression: updateExpression,
+      ConditionExpression: conditionExpression,
+      ExpressionAttributeNames: expressionAttributeNames,
+      ExpressionAttributeValues: expressionAttributeValues
+    }
+
+    return this.dynamodb().updateItem(params).promise()
   }
 
-  async upsert () {
+  async upsert (o) {
+    const item = this.itemSchema.toDynamo(o)
+    const expressionAttributeNames = {}
+    const expressionAttributeValues = {}
+    let updateExpression = ''
 
+    Object.keys(item).forEach((name) => {
+      let key = this.makeKey()
+      while (expressionAttributeNames.hasOwnProperty(`#${key}`)) {
+        key = this.makeKey()
+      }
+
+      if (!this.keySchema.hasOwnProperty(name)) {
+        expressionAttributeNames[`#${key}`] = name
+        expressionAttributeValues[`:${key}`] = item[name]
+
+        if (!updateExpression) {
+          updateExpression = `SET #${key} = :${key}`
+        } else {
+          updateExpression += `, #${key} = :${key}`
+        }
+      }
+    })
+
+    const params = {
+      Key: this.keySchema.toDynamo(o),
+      TableName: this.tableName,
+      UpdateExpression: updateExpression,
+      ExpressionAttributeNames: expressionAttributeNames,
+      ExpressionAttributeValues: expressionAttributeValues
+    }
+
+    return this.dynamodb().updateItem(params).promise()
   }
 
   dynamodb () {
     return dynamodb
   }
-}
 
-function makeKey (length = 5) {
-  let text = ''
-  let possible = 'abcdefghijklmnopqrstuvwxyz'
+  makeKey (length = 5) {
+    let text = ''
+    let possible = 'abcdefghijklmnopqrstuvwxyz'
 
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return text
   }
-
-  return text
 }
